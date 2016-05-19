933c4f90b2d89e49e36e71b9990cce4dc1864db8
Lukas Ruebbelke <simpul@gmail.com>
2016-05-19 16:45:40 -0400

    beast mode!

--- src/app/account/account.controller.js
+++ src/app/account/account.controller.js
@@ -2,12 +2,26 @@ function AccountController() {
   $ctrl = this;
   $ctrl.currentAccount = {
     title: 'My current account yo!',
-    description: 'This is a description'
+    description: 'This is a description',
+    active: true,
+    total: 10000000000000
   }
 
-  $ctrl.kickTheJams = function(jam) {
+  $ctrl.kickTheJams = kickTheJams;
+  $ctrl.showAccount = showAccount;
+  $ctrl.isShown = $ctrl.showAccount($ctrl.currentAccount);
+
+  //
+
+  function kickTheJams (jam) {
     alert('DANCE PARTY WITH' + jam + '!');
-  };
+  }
+
+  function showAccount (account) {
+    return account.active && account.total > 1000;
+  }
+
+
 }
 
 angular.module('account')
